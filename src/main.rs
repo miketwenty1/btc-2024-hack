5228449c3b9c78e5bfcdf889d9f38874407e5d77
Michael Tidwell <tidwell@zebedee.io>
2024-07-16 20:14:38 -0400

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,242 @@
 use actix_files::NamedFile;
+use actix_web::error::ErrorBadRequest;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::env;
 use std::path::PathBuf;
+
+#[derive(Debug, Deserialize)]
+struct Label {
+    label: String,
+}
+
+async fn list_addresses_by_label(
+    client: &Client,
+    label: &str,
+    rpc_url: &str,
+    rpc_user: &str,
+    rpc_pass: &str,
+) -> Result<Vec<String>, reqwest::Error> {
+    let rpc_request = serde_json::json!({
+        "jsonrpc": "1.0",
+        "id": "listaddressesbylabel",
+        "method": "getaddressesbylabel",
+        "params": [label]
+    });
+
+    let response = client
+        .post(rpc_url)
+        .basic_auth(rpc_user, Some(rpc_pass))
+        .header("Content-Type", "application/json")
+        .json(&rpc_request)
+        .send()
+        .await?;
+
+    let response_json: serde_json::Value = response.json().await?;
+    let addresses = response_json["result"]
+        .as_object()
+        .unwrap()
+        .keys()
+        .cloned()
+        .collect::<Vec<_>>();
+    Ok(addresses)
+}
+
+async fn list_unspent_outputs(
+    client: &Client,
+    addresses: Vec<String>,
+    rpc_url: &str,
+    rpc_user: &str,
+    rpc_pass: &str,
+) -> Result<Vec<serde_json::Value>, reqwest::Error> {
+    let rpc_request = serde_json::json!({
+        "jsonrpc": "1.0",
+        "id": "listunspent",
+        "method": "listunspent",
+        "params": [0, 9999999, addresses]
+    });
+
+    let response = client
+        .post(rpc_url)
+        .basic_auth(rpc_user, Some(rpc_pass))
+        .header("Content-Type", "application/json")
+        .json(&rpc_request)
+        .send()
+        .await?;
+
+    let response_json: serde_json::Value = response.json().await?;
+    let utxos = response_json["result"].as_array().unwrap().clone();
+    Ok(utxos)
+}
+
+async fn create_and_send_raw_transaction(
+    client: &Client,
+    utxos: Vec<serde_json::Value>,
+    destination_address: &str,
+    fee: f64,
+    rpc_url: &str,
+    rpc_user: &str,
+    rpc_pass: &str,
+) -> Result<String, actix_web::error::Error> {
+    let mut inputs = Vec::new();
+    let mut total_amount = 0.0;
+
+    for utxo in &utxos {
+        let txid = utxo["txid"]
+            .as_str()
+            .ok_or_else(|| ErrorBadRequest("Missing txid"))?;
+        let vout = utxo["vout"]
+            .as_u64()
+            .ok_or_else(|| ErrorBadRequest("Missing vout"))?;
+        let amount = utxo["amount"]
+            .as_f64()
+            .ok_or_else(|| ErrorBadRequest("Missing amount"))?;
+        total_amount += amount;
+
+        inputs.push(json!({ "txid": txid, "vout": vout }));
+    }
+
+    let send_amount = total_amount - fee;
+    if send_amount <= 0.0 {
+        return Err(ErrorBadRequest(
+            "Insufficient funds to cover the transaction fee",
+        ));
+    }
+
+    let outputs = json!({ destination_address: send_amount });
+
+    let rpc_request = json!({
+        "jsonrpc": "1.0",
+        "id": "createrawtransaction",
+        "method": "createrawtransaction",
+        "params": [inputs, outputs]
+    });
+
+    let raw_tx_response = client
+        .post(rpc_url)
+        .basic_auth(rpc_user, Some(rpc_pass))
+        .header("Content-Type", "application/json")
+        .json(&rpc_request)
+        .send()
+        .await
+        .map_err(|e| ErrorBadRequest(e.to_string()))?;
+
+    let raw_tx_json: serde_json::Value = raw_tx_response
+        .json()
+        .await
+        .map_err(|e| ErrorBadRequest(e.to_string()))?;
+    let raw_tx = raw_tx_json["result"]
+        .as_str()
+        .ok_or_else(|| ErrorBadRequest("Failed to create raw transaction"))?;
+
+    let sign_rpc_request = json!({
+        "jsonrpc": "1.0",
+        "id": "signrawtransactionwithwallet",
+        "method": "signrawtransactionwithwallet",
+        "params": [raw_tx]
+    });
+
+    let sign_response = client
+        .post(rpc_url)
+        .basic_auth(rpc_user, Some(rpc_pass))
+        .header("Content-Type", "application/json")
+        .json(&sign_rpc_request)
+        .send()
+        .await
+        .map_err(|e| ErrorBadRequest(e.to_string()))?;
+
+    let sign_json: serde_json::Value = sign_response
+        .json()
+        .await
+        .map_err(|e| ErrorBadRequest(e.to_string()))?;
+    let signed_tx = sign_json["result"]["hex"]
+        .as_str()
+        .ok_or_else(|| ErrorBadRequest("Failed to sign transaction"))?;
+
+    let send_rpc_request = json!({
+        "jsonrpc": "1.0",
+        "id": "sendrawtransaction",
+        "method": "sendrawtransaction",
+        "params": [signed_tx]
+    });
+
+    let send_response = client
+        .post(rpc_url)
+        .basic_auth(rpc_user, Some(rpc_pass))
+        .header("Content-Type", "application/json")
+        .json(&send_rpc_request)
+        .send()
+        .await
+        .map_err(|e| ErrorBadRequest(e.to_string()))?;
+
+    let send_json: serde_json::Value = send_response
+        .json()
+        .await
+        .map_err(|e| ErrorBadRequest(e.to_string()))?;
+    let txid = send_json["result"]
+        .as_str()
+        .ok_or_else(|| ErrorBadRequest("Failed to send transaction"))?
+        .to_string();
+
+    Ok(txid)
+}
+#[derive(Debug, Deserialize)]
+struct SendAllRequest {
+    label: String,
+    destination_address: String,
+    fee: f64,
+}
+
+async fn send_all(
+    client: web::Data<Client>,
+    rpc_url: web::Data<String>,
+    req: web::Json<SendAllRequest>,
+) -> impl Responder {
+    let rpc_user = "marachain";
+    let rpc_pass = "marachain";
+
+    // List addresses by label
+    let addresses =
+        match list_addresses_by_label(&client, &req.label, &rpc_url, rpc_user, rpc_pass).await {
+            Ok(addresses) => addresses,
+            Err(e) => {
+                return HttpResponse::InternalServerError()
+                    .body(format!("Failed to list addresses: {}", e))
+            }
+        };
+
+    // List unspent outputs
+    let utxos = match list_unspent_outputs(&client, addresses, &rpc_url, rpc_user, rpc_pass).await {
+        Ok(utxos) => utxos,
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .body(format!("Failed to list unspent outputs: {}", e))
+        }
+    };
+
+    // Create and send raw transaction
+    let txid = match create_and_send_raw_transaction(
+        &client,
+        utxos,
+        &req.destination_address,
+        req.fee,
+        &rpc_url,
+        rpc_user,
+        rpc_pass,
+    )
+    .await
+    {
+        Ok(txid) => txid,
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .body(format!("Failed to create and send transaction: {}", e))
+        }
+    };
+
+    HttpResponse::Ok().body(txid)
+}
 
 async fn index() -> impl Responder {
     println!("Accessed /");
@@ -141,6 +374,7 @@ async fn main() -> std::io::Result<()> {
             .route("/new-address", web::post().to(get_new_address))
             .route("/new-code", web::get().to(get_new_code))
             .route("/accept", web::post().to(accept_address_and_code))
+            .route("/send-all", web::post().to(send_all))
             .service(actix_files::Files::new("/static", "./static").show_files_listing())
     })
     .bind("0.0.0.0:8080")?
